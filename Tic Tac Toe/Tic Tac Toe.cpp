6483cd62a08e22c558096a6012960ed25dae04b7
Yulia2120 <ket-dz@rambler.ru>
2021-08-09 20:12:22 +0300

    функция ввода(Input)

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -2,14 +2,16 @@
 //
 #include <iostream>
 #include <conio.h>
+#include <random>
+#include <time.h>
+
 
 void instruction();
 
 	char nameOne[30];
 	char nameTwo[30];     
-	char board[3][3];    //00 01 02
-				        //10 11 12
-				       //20 21 22
+	char board[3][3];    
+	bool step;
 
 int main()
 {
@@ -17,10 +19,16 @@ int main()
 
 	instruction();
 	system("cls");
-
-
-
-
+	int i = 0;
+	std::cout << "Введите имя 1 игрока: ";
+	std::cin.getline(nameOne, 30);
+	std::cout << "Введите имя 2 игрока: ";
+	std::cin.getline(nameTwo, 30);
+//случайным образом выьираем кто ходит первым
+	srand(time(NULL));
+	if (rand() & 1)    //битовое умножение на 1(деление с остатком на 2)
+		step = true;
+	else step = false;
 
 
 
@@ -55,5 +63,52 @@ void instruction()
 	std::cout << "\nДля хода нажмите цифру ячейки поля\n";
 	std::cout << "Для начала игры нажмите клавишу:";
 	_getch();
+}
 
+bool Input()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << "| " << board[i][j] << ' ';
+		}
+		std::cout << '|';
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+	if (step)
+		std::cout << "Ходит " << nameOne << " : ";
+	else std::cout << "Ходит " << nameTwo << " : ";
+
+	int n;
+	std::cin >> n;
+	if (n < 1 || n > 9)
+		return false;   //ввод не удался
+	int i, j;  //ищем индексы столбца и строки
+											   //00 01 02
+						                      //10 11 12
+					                          //20 21 22
+	if (n % 3 == 0)       //третий столбец
+	{
+		i = n / 3 - 1;   //строки
+		j = 2;
+	}
+	else
+	{
+		j = n % 3 - 1;
+		i = n / 3;
+	}
+
+	if (board[i][j] == 'O' || board[i][j] == 'X') //если клетка занята, то ввод не удался
+		return false;
+
+	if (step)     //смотря чей ход
+	{
+		board[i][j] = 'X';
+		step = false;
+	}
+	else { board[i][j] = 'O'; step = true; }
+	return true;
 }
